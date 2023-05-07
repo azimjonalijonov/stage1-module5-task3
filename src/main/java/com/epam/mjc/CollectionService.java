6a72c5c5d59d9d63d46601e14117f2f6025cdf96
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-05-07 18:03:21 +0500

    Update CollectionService.java

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,54 @@
 package com.epam.mjc;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<Integer> str = list
+                .stream()
+                .filter(b -> b % 2 == 0)
+                .collect(Collectors.toList());
+        return str;
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> str = list
+                .stream()
+                .map(c -> c.toUpperCase())
+                .collect(Collectors.toList());
+        return str;
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+       int  optionalInteger = list
+               .stream()
+               .mapToInt(v -> v)
+               .max().orElseThrow(NoSuchElementException::new);
+       return Optional.of(optionalInteger);
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        int op = list
+                .stream()
+                .mapToInt(v -> {
+                    return v.stream()
+                            .mapToInt(c -> c)
+                            .min().orElseThrow(NoSuchElementException::new);
+
+                })
+                .min().orElseThrow(NoSuchElementException::new);
+        return Optional.of(op);
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        int res = list
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        return res;
     }
 }
